2d7c94dd46694f0c9dc36baab137b08da9ff41ec
nathants <me@nathants.com>
2018-02-26 20:52:04 -0800

    prices and histograms for ec2

--- aws_util.py
+++ aws_util.py
@@ -79,3 +79,26 @@ def setup():
         sys.exit(1)
     except:
         raise
+
+def region():
+    boto3.client('ec2') # run session setup logic
+    return boto3.DEFAULT_SESSION.region_name
+
+region_names = {
+    "us-east-2": "US East (Ohio)",
+    "us-east-1": "US East (N. Virginia)",
+    "us-west-1": "US West (N. California)",
+    "us-west-2": "US West (Oregon)",
+    "ap-south-1": "Asia Pacific (Mumbai)",
+    "ap-northeast-2": "Asia Pacific (Seoul)",
+    "ap-southeast-1": "Asia Pacific (Singapore)",
+    "ap-southeast-2": "Asia Pacific (Sydney)",
+    "ap-northeast-1": "Asia Pacific (Tokyo)",
+    "ca-central-1": "Canada (Central)",
+    "cn-north-1": "China (Beijing)",
+    "eu-central-1": "EU (Frankfurt)",
+    "eu-west-1": "EU (Ireland)",
+    "eu-west-2": "EU (London)",
+    "eu-west-3": "EU (Paris)",
+    "sa-east-1": "South America (São Paulo)",
+}
