868e4cebcff0561859babc8168cec52f01b582f3
nathants <me@nathants.com>
2018-03-03 22:13:56 -0800

    env checking for test script

--- test.sh
+++ test.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
+if ! which aws-lambda-deploy; then
+    echo please install cli-aws before running tests: python3 setup.py develop 1>&2
+    exit 1
+fi
+
+if ! which py.test; then
+    echo please install py.test: pip3 install pytest 1>&2
+    exit 1
+fi
+
 cd $(dirname $(realpath $0))
 
 for example in examples/*.py; do
