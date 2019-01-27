b79f38787581cb8ab5c01bc7174ca74fba1e80aa
nathants <me@nathants.com>
2019-01-26 20:23:02 -0800

    lambda can include static files

---
+++ examples/lambda_includes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.7
+#
+# policy: CloudWatchLogsFullAccess
+# include: include_me.txt
+
+import os
+
+def main(event, context):
+    """
+    >>> import shell, uuid
+    >>> run = lambda *a, **kw: shell.run(*a, stream=True, **kw)
+    >>> path = 'examples/lambda_includes.py'
+    >>> uid = str(uuid.uuid4())
+
+    >>> _ = run(f'aws-lambda-deploy {path} SOME_UUID={uid} -y')
+
+    >>> assert f'"data123 {uid}"' == run(f'aws-lambda-invoke {path}')
+
+    """
+    with open('include_me.txt') as f:
+        return f'{f.read().strip()} {os.environ["SOME_UUID"]}'
