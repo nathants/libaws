33e9a73d1c1f74f4a71ee60083c6680c0ddc1200
nathants <me@nathants.com>
2021-03-23 22:25:59 -0700

    size signal handler channel

--- lib/lib.go
+++ lib/lib.go
@@ -36,7 +36,7 @@ func (LogWriter) Write(p []byte) (int, error) {
 var Logger = log.New(LogWriter{}, "", log.Llongfile)
 
 func SignalHandler(cancel func()) {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
