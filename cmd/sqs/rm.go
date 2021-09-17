b3c9efc08400fb55bf66eadb4966eaa49ecafb0c
nathants <me@nathants.com>
2021-09-16 23:49:42 -1000

    sqs-rm

---
+++ cmd/sqs/rm.go
@@ -0,0 +1,39 @@
+package cliaws
+
+import (
+	"context"
+
+	"github.com/alexflint/go-arg"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/nathants/cli-aws/lib"
+)
+
+func init() {
+	lib.Commands["sqs-rm"] = sqsRm
+	lib.Args["sqs-rm"] = sqsRmArgs{}
+}
+
+type sqsRmArgs struct {
+	QueueName string `arg:"positional,required"`
+}
+
+func (sqsRmArgs) Description() string {
+	return "\ndelete an sqs queue\n"
+}
+
+func sqsRm() {
+	var args sqsRmArgs
+	arg.MustParse(&args)
+	ctx := context.Background()
+	queueUrl, err := lib.SQSQueueUrl(ctx, args.QueueName)
+	if err != nil {
+		lib.Logger.Fatal("error: ", err)
+	}
+	_, err = lib.SQSClient().DeleteQueueWithContext(ctx, &sqs.DeleteQueueInput{
+		QueueUrl: aws.String(queueUrl),
+	})
+	if err != nil {
+		lib.Logger.Fatal("error: ", err)
+	}
+}
