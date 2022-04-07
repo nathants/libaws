709ee0629f28a0f6c9e9df4ac22ac50ca8fe01e6
nathants <me@nathants.com>
2022-04-06 22:03:42 -1000

    bash completion for creds should filter

--- completions.d/aws-creds.sh
+++ completions.d/aws-creds.sh
@@ -7,7 +7,7 @@ fi
 
 _aws_creds () {
 	COMPREPLY=($(
-                   ls ~/.aws_creds | grep -v ^_ | sed s/\.sh$// | grep "^${COMP_WORDS[$COMP_CWORD]}"
+                   ls ~/.aws_creds | grep -v ^_ | grep '.sh$' | sed s/\.sh$// | grep "^${COMP_WORDS[$COMP_CWORD]}"
                ))
 }
 
