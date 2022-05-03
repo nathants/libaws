5d3d132bcb64d8a6209a57b1e2ab9d1e5d05870f
nathants <me@nathants.com>
2022-05-02 20:39:04 -1000

    cost-explorer should cache data locally by the hour, this api is EXPENSIVE

--- cmd/cost/explorer.go
+++ cmd/cost/explorer.go
@@ -2,7 +2,10 @@ package cliaws
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
+	"path"
 	"regexp"
 	"strings"
 	"time"
@@ -20,92 +23,119 @@ func init() {
 
 type costExplorerArgs struct {
 	AccountNum   string `arg:"required,positional"`
-	DaysAgoStart int    `arg:"-d,--days-ago-start" default:"7"`
-	Region       string `arg:"-r,--region"`
+	DaysAgoStart int    `arg:"-d,--days-ago-start" default:"14"`
 }
 
 func (costExplorerArgs) Description() string {
-	return "\ncost explorer\n"
+	return "\ncost explorer, caching data locally by hour since the api is very expensive\n"
+}
+
+func cachePath(accountNum string, hourStartTime time.Time) string {
+	home := os.Getenv("HOME")
+	if home == "" {
+		panic("$HOME is empty string")
+	}
+	return fmt.Sprintf("%s/.cli-aws/cost-explorer-cache/%s/%s.json", home, accountNum, hourStartTime.Format(time.RFC3339))
 }
 
 func costExplorer() {
 	var args costExplorerArgs
 	arg.MustParse(&args)
 	ctx := context.Background()
-	var token *string
-	var results []*costexplorer.ResultByTime
-	filters := []*costexplorer.Expression{
-		{
-			Dimensions: &costexplorer.DimensionValues{
-				Key:    aws.String(costexplorer.DimensionLinkedAccount),
-				Values: []*string{aws.String(args.AccountNum)},
-			},
-		},
-	}
-	if args.Region != "" {
-		filters = append(filters, &costexplorer.Expression{
-			Dimensions: &costexplorer.DimensionValues{
-				Key:    aws.String(costexplorer.DimensionRegion),
-				Values: []*string{aws.String(args.Region)},
-			},
-		})
-	}
-	var filter *costexplorer.Expression
-	if len(filters) == 1 {
-		filter = filters[0]
-	} else {
-		filter = &costexplorer.Expression{And: filters}
-	}
+	start := time.Now().UTC().Add(time.Duration(args.DaysAgoStart) * -1 * 24 * time.Hour).Round(time.Hour)
+	end := start
 	for {
+		start = end
+		end = end.Add(1 * time.Hour)
+		if end.After(time.Now().UTC()) {
+			break
+		}
+		pth := cachePath(args.AccountNum, start)
+		if lib.Exists(pth) {
+			continue
+		}
 		out, err := lib.CostExplorerClient().GetCostAndUsageWithContext(ctx, &costexplorer.GetCostAndUsageInput{
-			Filter:      filter,
-			Granularity: aws.String(costexplorer.GranularityHourly),
-			GroupBy: []*costexplorer.GroupDefinition{
-				{Type: aws.String(costexplorer.GroupDefinitionTypeDimension), Key: aws.String(costexplorer.DimensionService)},
+			Filter: &costexplorer.Expression{
+				Dimensions: &costexplorer.DimensionValues{
+					Key:    aws.String(costexplorer.DimensionLinkedAccount),
+					Values: []*string{aws.String(args.AccountNum)},
+				},
 			},
+			Granularity: aws.String(costexplorer.GranularityHourly),
+			GroupBy: []*costexplorer.GroupDefinition{{
+				Type: aws.String(costexplorer.GroupDefinitionTypeDimension),
+				Key:  aws.String(costexplorer.DimensionService),
+			}},
 			Metrics: []*string{
 				aws.String(costexplorer.MetricUnblendedCost),
 			},
-			NextPageToken: token,
 			TimePeriod: &costexplorer.DateInterval{
-				End:   aws.String(time.Now().UTC().Format(time.RFC3339)),
-				Start: aws.String(time.Now().UTC().Add(time.Duration(args.DaysAgoStart) * -1 * 24 * time.Hour).Format(time.RFC3339)),
+				Start: aws.String(start.Format(time.RFC3339)),
+				End:   aws.String(end.Format(time.RFC3339)),
 			},
 		})
 		if err != nil {
 			lib.Logger.Fatal("error: ", err)
 		}
-		// dimensions = append(dimensions, out.DimensionValueAttributes...)
-		// groups = append(groups, out.GroupDefinitions...)
-		results = append(results, out.ResultsByTime...)
-		if out.NextPageToken == nil {
+		if out.NextPageToken != nil {
+			panic("should not be paginating")
+		}
+		data, err := json.Marshal(out.ResultsByTime)
+		if err != nil {
+			panic(err)
+		}
+		err = os.MkdirAll(path.Dir(pth), os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+		err = os.WriteFile(pth, data, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+		lib.Logger.Println("cached data for:", pth)
+	}
+	dashes := regexp.MustCompile(`\-+`)
+	start = time.Now().UTC().Add(time.Duration(args.DaysAgoStart) * -1 * 24 * time.Hour).Round(time.Hour)
+	end = start
+	for {
+		start = end
+		end = end.Add(1 * time.Hour)
+		if end.After(time.Now().UTC()) {
 			break
 		}
-		token = out.NextPageToken
-	}
-	// fmt.Println(lib.Pformat(dimensions))
-	// fmt.Println(lib.Pformat(groups))
-	// fmt.Println(lib.Pformat(results))
-	dashes := regexp.MustCompile(`\-+`)
-	for _, result := range results {
-		var vals []string
-		for _, group := range result.Groups {
-			if len(group.Keys) != 1 {
-				panic(lib.PformatAlways(group))
-			}
-			key := *group.Keys[0]
-			key = strings.ReplaceAll(key, "AWS ", "")
-			key = strings.ReplaceAll(key, "Amazon Simple Storage Service", "S3")
-			key = strings.ReplaceAll(key, "Amazon EC2 Container Registry (ECR)", "ECR")
-			key = strings.ReplaceAll(key, "Amazon ", "")
-			key = strings.ReplaceAll(key, "Simple Queue Service ", "SQS")
-			key = strings.ReplaceAll(key, "API Gateway", "ApiGateway")
-			key = strings.ReplaceAll(key, "AmazonCloudWatch", "Cloudwatch")
-			key = strings.ReplaceAll(key, " ", "-")
-			key = dashes.ReplaceAllString(key, "-")
-			key = strings.ReplaceAll(key, "Elastic-Compute-Cloud-Compute", "EC2")
-			vals = append(vals, fmt.Sprintf("%s=%s", key, *group.Metrics["UnblendedCost"].Amount))
+		pth := cachePath(args.AccountNum, start)
+		if !lib.Exists(pth) {
+			panic("data should exist at this point")
 		}
-		fmt.Println("timestamp="+*result.TimePeriod.Start, strings.Join(vals, " "))
+		results := []*costexplorer.ResultByTime{}
+		data, err := os.ReadFile(pth)
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal(data, &results)
+		if err != nil {
+			panic(err)
+		}
+		for _, result := range results {
+			var vals []string
+			for _, group := range result.Groups {
+				if len(group.Keys) != 1 {
+					panic(lib.PformatAlways(group))
+				}
+				key := *group.Keys[0]
+				key = strings.ReplaceAll(key, "AWS ", "")
+				key = strings.ReplaceAll(key, "Amazon Simple Storage Service", "S3")
+				key = strings.ReplaceAll(key, "Amazon EC2 Container Registry (ECR)", "ECR")
+				key = strings.ReplaceAll(key, "Amazon ", "")
+				key = strings.ReplaceAll(key, "Simple Queue Service ", "SQS")
+				key = strings.ReplaceAll(key, "API Gateway", "ApiGateway")
+				key = strings.ReplaceAll(key, "AmazonCloudWatch", "Cloudwatch")
+				key = strings.ReplaceAll(key, " ", "-")
+				key = dashes.ReplaceAllString(key, "-")
+				key = strings.ReplaceAll(key, "Elastic-Compute-Cloud-Compute", "EC2")
+				vals = append(vals, fmt.Sprintf("%s=%s", key, *group.Metrics["UnblendedCost"].Amount))
+			}
+			fmt.Println("timestamp="+*result.TimePeriod.Start, strings.Join(vals, " "))
+		}
 	}
 }
