0bcad13899559abf5855bfa7e618109e7076187c
nathants <me@nathants.com>
2022-07-03 23:02:58 -1000

    add events-ls-buses

---
+++ cmd/events/ls_buses.go
@@ -0,0 +1,34 @@
+package cliaws
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/alexflint/go-arg"
+	"github.com/nathants/libaws/lib"
+)
+
+func init() {
+	lib.Commands["events-ls-buses"] = eventsLsBuses
+	lib.Args["events-ls-buses"] = eventsLsBusesArgs{}
+}
+
+type eventsLsBusesArgs struct {
+}
+
+func (eventsLsBusesArgs) Description() string {
+	return "\nlist event buses\n"
+}
+
+func eventsLsBuses() {
+	var args eventsLsBusesArgs
+	arg.MustParse(&args)
+	ctx := context.Background()
+	buses, err := lib.EventsListBuses(ctx)
+	if err != nil {
+		lib.Logger.Fatal("error: ", err)
+	}
+	for _, bus := range buses {
+		fmt.Println(*bus.Name)
+	}
+}
