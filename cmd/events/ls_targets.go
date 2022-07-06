edddffc207abc62570903346a1f466a5105771a7
nathants <me@nathants.com>
2022-07-05 23:58:13 -1000

    add events-ls-targets

---
+++ cmd/events/ls_targets.go
@@ -0,0 +1,41 @@
+package cliaws
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/alexflint/go-arg"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/nathants/libaws/lib"
+)
+
+func init() {
+	lib.Commands["events-ls-targets"] = eventsLsTargets
+	lib.Args["events-ls-targets"] = eventsLsTargetsArgs{}
+}
+
+type eventsLsTargetsArgs struct {
+	RuleName string `arg:"positional,required"`
+	BusName  string `arg:"positional" default:"" help:"specify name to target a bus other than the default bus"`
+}
+
+func (eventsLsTargetsArgs) Description() string {
+	return "\nlist event targets\n"
+}
+
+func eventsLsTargets() {
+	var args eventsLsTargetsArgs
+	arg.MustParse(&args)
+	var busName *string
+	if args.BusName != "" {
+		busName = aws.String(args.BusName)
+	}
+	ctx := context.Background()
+	targets, err := lib.EventsListRuleTargets(ctx, args.RuleName, busName)
+	if err != nil {
+		lib.Logger.Fatal("error: ", err)
+	}
+	for _, target := range targets {
+		fmt.Println(lib.Pformat(target))
+	}
+}
