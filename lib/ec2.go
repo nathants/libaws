af87a00a0835defefe9327e590f348067804f5f3
nathants <me@nathants.com>
2023-10-21 22:57:33 -1000

    add alpine 3.18.4 constant

--- lib/ec2.go
+++ lib/ec2.go
@@ -48,6 +48,7 @@ const (
 	EC2AmiAlpine3160 = "alpine-3.16.0"
 	EC2AmiAlpine3182 = "alpine-3.18.2"
 	EC2AmiAlpine3183 = "alpine-3.18.3"
+	EC2AmiAlpine3184 = "alpine-3.18.4"
 )
 
 var ec2RegexpAlpine = regexp.MustCompile(`alpine\-\d\d?\.\d\d?\.\d\d?`)
