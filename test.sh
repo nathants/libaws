329d258a900fac48d07670a831d473d6e1057e4c
nathants <me@nathants.com>
2025-04-08 14:17:31 -1000

    rename misc

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ make
 (cd examples/simple/go;     for name in *; do (cd $name && timeout 600 python test.py); done)
 (cd examples/simple/docker; for name in *; do (cd $name && timeout 600 python test.py); done)
 (cd examples/complex;       for name in *; do (cd $name && timeout 600 python test.py); done)
-(cd examples/_misc;         for name in *; do (cd $name && timeout 600 python test.py); done)
+(cd examples/misc;          for name in *; do (cd $name && timeout 600 python test.py); done)
 (
     cd lib
     nontest=$(ls *.go | grep -v _test.go)
